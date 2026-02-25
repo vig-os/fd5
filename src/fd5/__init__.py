8cb07208017a34113c81c6803621a7a3f4d2cbf8
gerchowl <lars.gerchow@gmail.com>
2026-02-25 08:37:14 +0100

    feat(migrate): export migrate from fd5 package __init__
    Refs: #87

--- src/fd5/__init__.py
+++ src/fd5/__init__.py
@@ -4,7 +4,8 @@ __version__ = "0.1.0"
 
 from fd5.create import create
 from fd5.hash import verify
+from fd5.migrate import migrate
 from fd5.naming import generate_filename
 from fd5.schema import validate
 
-__all__ = ["create", "generate_filename", "validate", "verify"]
+__all__ = ["create", "generate_filename", "migrate", "validate", "verify"]
